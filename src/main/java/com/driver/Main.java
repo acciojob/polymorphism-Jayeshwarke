b68ddfd3fb93a4193c9c2a97363c907b84564a1c
Dewayani <dewayaniwarake@gmail.com>
2023-07-22 10:44:53 +0530

    Here is the polymorphism assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,36 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        Product p = new Product();
+
+        int result1 = p.product(5,2);
+        System.out.println(result1);
+
+        int result2 = p.product(5,2,3);
+        System.out.println(result2);
+
+        double result3 = p.product(5,2);
+        System.out.println(result3);
+
+
+    }
+
+    public static class Product{
+        public int product(int x, int y){
+            return x*y;
+
+        }
+
+        public int product(int x, int y, int z){
+            return x*y*z;
+
+        }
+
+        public double product(double x, double y){
+            return x*y;
+
+        }
+    }
 
 }
---
+++ src/main/java/com/driver/product.java
@@ -0,0 +1,17 @@
+package com.driver;
+
+public class product {
+    int x;
+
+    int y;
+
+    public  product(int x, int y){
+        this.x =x;
+        this.y=y;
+    }
+
+    public void product(int x, int y){
+
+    }
+
+}
